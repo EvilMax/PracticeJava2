a4583f6319ec1096199cb31b92b4a93d05a90ecb
max <evilmax@gmail.com>
2024-02-18 21:15:29 +0300

    More consistency changes

--- src/org/practice/scala/Main.java
+++ src/org/practice/scala/Main.java
@@ -1,12 +1,10 @@
 package org.practice.scala;
 
-import org.practice.scala.domain.Handler;
 import org.practice.scala.impl.ClientImpl;
 import org.practice.scala.impl.HandlerImpl;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,8 +12,11 @@ public class Main {
         int capacity = Runtime.getRuntime().availableProcessors() * 2 + 1;
         // Фиксированный размер - чтобы складывались задачи в очередь, если будет "зашиваться", а не порождались потоки
         // По-идее, при перегрузке так позже упадём с Out of memory
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(capacity);
+        ExecutorService executor = Executors.newFixedThreadPool(capacity);
         HandlerImpl handler = new HandlerImpl(new ClientImpl(), executor);
-
+        for (int i = 0; i < 10000; i++) {
+            handler.performOperation();
+        }
+        executor.shutdown();
     }
 }
